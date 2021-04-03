804d158ca71b3d5bdb5e8991a4a42015ee4cba50
Yuguo Zhao <cheerzhao77@gmail.com>
2021-04-02 20:18:12 -0500

    add headerStyle to stack

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, AppRegistry, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  TouchableOpacity,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Home = ({ navigation }) => (
   <View>
     <Text>This is the Home view</Text>
-    <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
+    <TouchableOpacity onPress={() => navigation.navigate("Dashboard")}>
       <Text>Press here for the Dashboard</Text>
     </TouchableOpacity>
   </View>
@@ -25,19 +31,22 @@ const Stack = createStackNavigator();
 function MyStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        tabBarOptions={{
-          activeTintColor: "tomato",
-        }}
-      >
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={Home}
+          options={{ title: "Homie" }}
         />
         <Stack.Screen
           name="Dashboard"
           component={Dashboard}
+          options={{
+            title: "Dashboard",
+            headerTintColor: "red",
+            headerStyle: {
+              backgroundColor: "green",
+            },
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
