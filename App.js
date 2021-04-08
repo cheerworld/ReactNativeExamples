68ce02de8bff349d78c9166df89cc4e831677179
Yuguo Zhao <cheerzhao77@gmail.com>
2021-04-07 22:54:07 -0500

    feat: use Animated from react-native

--- App.js
+++ App.js
@@ -5,58 +5,46 @@ import {
   View,
   AppRegistry,
   TouchableOpacity,
+  Animated,
 } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const Home = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>This is the Home view</Text>
-    <TouchableOpacity onPress={() => navigation.openDrawer()}>
-      <Text>Open Drawer</Text>
-    </TouchableOpacity>
-  </View>
-);
+export default class App extends React.Component {
+  state = {
+    opacity: new Animated.Value(0),
+    width: new Animated.Value(0),
+    height: new Animated.Value(0),
+  };
 
-const Dashboard = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>This is the Dashboard</Text>
-    <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-      <Text>Toggle Drawer</Text>
-    </TouchableOpacity>
-  </View>
-);
-
-const Drawer = createDrawerNavigator();
-
-function MyStack() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen
-          name="Home"
-          component={Home}
-          options={{
-            drawerLabel: "HOMEIE",
-            drawerIcon: () => <FontAwesome name="home" size={20} color="red" />,
+  componentDidMount() {
+    const { opacity, width, height } = this.state;
+    Animated.timing(opacity, { toValue: 1, duration: 1000 }).start();
+    Animated.spring(width, { toValue: 300, speed: 5 }).start();
+    Animated.spring(height, { toValue: 300, speed: 5 }).start();
+  }
+  render() {
+    const { opacity, width, height } = this.state;
+    return (
+      <View style={styles.container}>
+        <Animated.Image
+          style={[styles.img, { opacity, height, width }]}
+          source={{
+            uri:
+              "https://pbs.twimg.com/profile_images/452111932548726784/EOKCqhMK_400x400.jpeg",
           }}
         />
-        <Drawer.Screen
-          name="Dashboard"
-          component={Dashboard}
-          options={{
-            drawerLabel: "DASHBOARDIE",
-            drawerIcon: () => (
-              <FontAwesome name="dashboard" size={20} color="red" />
-            ),
-          }}
-        />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+      </View>
+    );
+  }
 }
 
-export default MyStack;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: 200,
+    height: 200,
+  },
+});
